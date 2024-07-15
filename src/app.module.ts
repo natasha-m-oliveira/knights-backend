107a94e900f58f4b78223187f22f555803790e7b
natasha-m-oliveira <me@natashamo.dev>
2024-07-15 15:11:55 -0300

    fix: fix application Bootstrap setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from '@infra/http/http.module';
+import { GlobalModule } from '@infra/global.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule, GlobalModule],
 })
 export class AppModule {}
